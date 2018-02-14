0818a243fed3800046b3c69a557603501a7cb731
s0rrin0tsri <s0rrin0tsri@users.noreply.github.com>
2018-02-14 09:35:35 -0500

    User input (#1)
    * input from user on text to translate and prefered language
    
    * Update lang_basic.py
    
    * Update lang_basic.py
    
    * Update lang_basic.py
    
    * Update lang_basic.py

--- lang_basic.py
+++ lang_basic.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
-# To authenticate, download your json keyfile from your account.
-# Once done, run the following command in a terminal, or set the
-# export in your ~/.bashrc or ~/.profile
+# To authenticate, to Google you will need to 
+# download your json keyfile from your account.
+# Once done, run the following command in a terminal:
 # export GOOGLE_APPLICATION_CREDENTIALS="/path/to/keyfile.json"
+#
+# Alternatively, set the export in your ~/.bashrc or ~/.profile
+################################################################
 
 # Imports the Google Cloud client library
 from google.cloud import translate
@@ -11,12 +14,16 @@ from google.cloud import translate
 # Instantiates a client
 translate_client = translate.Client()
 
-# The text to translate
-text = u'Hello, world!'
-# The target language
-target = 'ar'
+# The text to translate provided by the user
+text = raw_input(u'Text to translate? ')
+# text = u'What do you mean?'
 
-# Translates some text into Arabic
+# The target language as a two letter language code
+# (e.g., en, ar, ru, fr, etc.)
+target = raw_input(u'Digraph for Language? ')
+# target = 'ar'
+
+# Translates text into chosen language
 translation = translate_client.translate(
     text,
     target_language=target)
